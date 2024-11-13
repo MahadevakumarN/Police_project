a151e04e8ee747e5d5db84b58c36197de4772495
Mahadeva <mahadevakumarn451@gmail.com>
2024-11-13 21:27:30 +0530

    update complete know

--- Demo.java
+++ Demo.java
@@ -4,12 +4,9 @@ public ststic void main(String[] args){
 int a = 10;
 int b = 20;
 int c = 30;
-<<<<<<< HEAD
-}
-}
-=======
   int d = 40; 
   int f  = a+b+c+d;
   System.out.println("Total values is " + f);
 }
->>>>>>> 6f04aaceb2d6f02b2ceef1b3b2ddeecefc8f9827
+}
+}
